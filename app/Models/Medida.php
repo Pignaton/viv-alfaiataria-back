e550b24554f63a8f40cd715d8b4c1592e20d113e
kaleb pignaton <kpignaton@ymail.com>
2025-06-14 10:40:31 -0300

    feat: cria modelo Medida para armazenar medidas dos usuários

---
+++ app/Models/Medida.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Medida extends Model
+{
+    use HasFactory;
+
+    public $timestamps = false;
+
+    const CREATED_AT = 'data_registro';
+
+    const UPDATED_AT = null;
+
+    protected $table = 'medida';
+
+    protected $fillable = [
+        'usuario_id',
+        'nome',
+        'valor',
+        'unidade',
+        'data_registro'
+    ];
+
+    protected $casts = [
+        'valor' => 'decimal:2',
+        'data_registro' => 'datetime'
+    ];
+
+    public function usuario()
+    {
+        return $this->belongsTo(Usuario::class, 'usuario_id', 'id');
+    }
+}
